72b144f47f47d274f7331cf11d014d04bcc22108
chrisk3 <chris.kaundart@gmail.com>
2013-11-15 19:03:18 -0800

    initial commit for parallax scrolling presentation app. current build features manually added slides and next/prev buttons for navigating the presentation.

---
+++ js/pres.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	// Cache the Window object
+	$window = $(window);
+
+	$('div[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+
+		$(window).scroll(function() {
+
+		// Scroll the background at var speed
+		// the yPos is a negative value because we're scrolling it UP!								
+		var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+
+		// Put together our final background position
+		var coords = '50% '+ yPos + 'px';
+
+		// Move the background
+		$bgobj.css({ backgroundPosition: coords });
+
+		}); // window scroll Ends
+
+	});
+
+	var slide = 1
+
+	$('button').on('click', function() {
+		if ($(this).attr('class') == 'prev') {
+			slide -= 1;
+			$('html, body').animate({
+        		scrollTop: $("#" + slide).offset().top
+    		}, 1500);
+		} else {
+			slide += 1;
+			$('html, body').animate({
+        		scrollTop: $("#" + slide).offset().top
+    		}, 1500);
+		}
+		console.log(slide);
+		// $('html, body').animate({
+  //       	scrollTop: $("#" + slide).offset().top
+  //   	}, 1500);
+	});
+
+});
